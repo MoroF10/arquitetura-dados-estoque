2cb74b41e89e7e64eb9a542bafd8e1be001e34df
Fernando Moro <99342443+MoroF10@users.noreply.github.com>
2026-02-19 11:45:42 -0300

    Update create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,13 @@
--- arquivo inicial
+/*Criando Tabela de identificação classes produtos*/
+CREATE TABLE core.classe_produto (
+	id_classe SERIAL PRIMARY KEY,
+	classe_produto VARCHAR(30),
+	ativo BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+/*Criando Tabela de tipos Produto(campanha, linha, descontinuado)*/
+CREATE TABLE core.tipo_produto (
+	id_tipo_produto SERIAL PRIMARY KEY,
+	tipo_produto VARCHAR(30) NOT NULL UNIQUE,
+	produto_controla_estoque BOOLEAN NOT NULL DEFAULT TRUE
+);
