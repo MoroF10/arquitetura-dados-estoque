2da851e3e459e43313364a20d740984066f21f7f
Fernando Moro <99342443+MoroF10@users.noreply.github.com>
2026-02-20 09:40:13 -0300

    Create Criando_tabelas_produtos.py

---
+++ python/Criando_tabelas_produtos.py
@@ -0,0 +1,6 @@
+#Importando aS bibliotecas usadas 
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sqlalchemy import create_engine, text
+from datetime import datetime
