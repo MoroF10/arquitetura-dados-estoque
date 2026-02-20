3d30f55dccb132e6980b80d502a055219620f0e7
Fernando Moro <99342443+MoroF10@users.noreply.github.com>
2026-02-20 11:28:49 -0300

    Update Criando_tabelas_produtos.py

--- python/Criando_tabelas_produtos.py
+++ python/Criando_tabelas_produtos.py
@@ -1,6 +1,48 @@
 #Importando aS bibliotecas usadas 
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sqlalchemy import create_engine, text
-from datetime import datetime
+
+
+# --- Configurações de conexão ---
+usuario = os.getenv("DB_USER")
+senha = os.getenv("DB_PASSWORD")
+host = os.getenv("DB_HOST")
+porta = os.getenv("DB_PORT")
+banco = os.getenv("DB_NAME")
+schema = "core"        # use o schema que você criou, ou None se for 'public'
+
+def conectar_banco():
+  engine = create_engine(
+    f"postgresql+psycopg2://{usuario}:{senha}@{host}:{porta}/{banco}")
+  return engine
+
+def ler_trata_csv(caminho):
+  df = pd.read_csv(caminho, sep=';')
+  df.columns = df.columns.str.strip()
+  df.dropna(inplace=True)
+  return df
+
+def carrega_dados(df, engine):
+  df.to_sql(
+    'produto', 
+    engine, 
+    if_exists='append',
+    schema='core', 
+    index=False)
+
+def main():
+  caminho = r''
+
+  engine = conectar_banco()
+  df = ler_trata_csv(caminho)
+  carrega_dados(df, engine)
+
+  print("Carga adicionada com sucesso!")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+caminho = (r'C:\Users\ferna\Cacaushow\Csv\Banco Novo\Produtos_core.csv')
