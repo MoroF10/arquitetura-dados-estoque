067c270cbe66723ae54ae97e6d87d4fd56bc4d59
Fernando Moro <99342443+MoroF10@users.noreply.github.com>
2026-03-05 06:23:46 -0300

    Add procedure to generate stock snapshot
    Create a procedure to insert new data into the stock table and handle conflicts.

---
+++ ETL/gera_snapshot_estoque.sql
@@ -0,0 +1,59 @@
+/*criando o procedure para inserir dados novos na tabela estoque*/
+CREATE OR REPLACE PROCEDURE dw.gerar_snapshot_estoque(p_data DATE)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    v_data_id_hoje INT;
+    v_data_id_ontem INT;
+BEGIN
+
+    -- Buscar data_id de hoje
+    SELECT data_id
+    INTO v_data_id_hoje
+    FROM dw.dim_calendario
+    WHERE data_ref = p_data;
+
+    -- Buscar data_id de ontem
+    SELECT data_id
+    INTO v_data_id_ontem
+    FROM dw.dim_calendario
+    WHERE data_ref = p_data - 1;
+
+    INSERT INTO dw.fato_estoque_snapshot (codigo_produto, data_id, quantidade)
+
+    SELECT
+        prev.codigo_produto,
+        v_data_id_hoje,
+        prev.quantidade + COALESCE(mov.movimentacao_dia, 0) AS saldo_final
+
+    FROM dw.fato_estoque_snapshot prev
+
+    LEFT JOIN
+    (
+        SELECT
+            m.codigo_produto,
+            SUM(
+                CASE
+                    WHEN m.id_tipo_movimentacao = 4 THEN m.quantidade
+                    WHEN m.id_tipo_movimentacao = 2 THEN -m.quantidade
+                    ELSE 0
+                END
+            ) AS movimentacao_dia
+        FROM core.movimento_estoque m
+        WHERE DATE(m.data_movimentacao) = p_data
+        GROUP BY m.codigo_produto
+    ) mov
+        ON mov.codigo_produto = prev.codigo_produto
+
+    WHERE prev.data_id = v_data_id_ontem
+
+    ON CONFLICT (codigo_produto, data_id)
+    DO UPDATE
+    SET quantidade = EXCLUDED.quantidade;
+
+END;
+$$;
+
+
+/*selecionando a data para executar o codigo*/
+CALL dw.gerar_snapshot_estoque('2026-03-03');
