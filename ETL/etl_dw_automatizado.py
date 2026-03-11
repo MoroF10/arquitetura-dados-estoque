f49bb36f6dda2221a3dfb4f3f58148254b3c1cad
Fernando Moro <99342443+MoroF10@users.noreply.github.com>
2026-03-11 11:16:29 -0300

    Refactor ETL function and improve error handling

--- ETL/etl_dw_automatizado.py
+++ ETL/etl_dw_automatizado.py
@@ -1,48 +1,64 @@
+import os
 from datetime import datetime
 import psycopg2
 
-print(f"Iniciando ETL: {datetime.now()}")
 
-conn = psycopg2.connect(
-        host = os.getenv("DB_HOST")
-        database= os.getenv("DB_NAME")
-        user= os.getenv("DB_USER")
-        password= os.getenv("DB_PASSWORD")
+def etl_dw():
+
+    conn = psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
     )
 
-def etl_dw()
+    cur = conn.cursor()
+
     try:
-        cur = conn.cursor()
+
         cur.execute("CALL dw.carga_completa();")
         conn.commit()
-    
+
         cur.execute("""
             SELECT MAX(data_id)
             FROM dw.fato_estoque_snapshot
         """)
         ultima_data = cur.fetchone()[0]
+
         print("Último snapshot gerado:", ultima_data)
-    
+
         cur.execute("""
             SELECT
                 MAX(data_id),
                 COUNT(*)
-            FROM dw.fato_estoque_snapshot;
+            FROM dw.fato_estoque_snapshot
         """)
-        qtd_inserida = cur.fetchone()[1]
-        print("Quantidade de registros:", qtd_inserida)
-    
+
+        _, qtd_registros = cur.fetchone()
+
+        print("Quantidade de registros:", qtd_registros)
+
         print(f"ETL finalizado: {datetime.now()}")
-    
-    
+
     except Exception as e:
-    
+
+        conn.rollback()
         print("Erro no ETL:", e)
-    
+
     finally:
-        try:
-    
+
         cur.close()
         conn.close()
+
+
+def main():
+
+    print(f"Iniciando ETL: {datetime.now()}")
+
+    etl_dw()
+
+
+if __name__ == "__main__":
+    main()
 
 
